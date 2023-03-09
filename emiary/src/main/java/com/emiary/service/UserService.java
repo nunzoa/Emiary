e5645945dff99b4e6bcd3a92261c4f3c5e8ec0f7
PHJ7729 <qkrgudwns1115@gmail.com>
2023-03-09 11:26:08 +0900

    회원가입 pull 신청

---
+++ emiary/src/main/java/com/emiary/service/UserService.java
@@ -0,0 +1,31 @@
+package com.emiary.service;
+
+import com.emiary.domain.User;
+
+/**
+ * 회원정보 관련 처리
+ */
+public interface UserService {
+	/**
+	 * 회원정보 저장 (가입)
+	 * @param member 가입양식에서 전달된 회원정보
+	 * @return 저장된 개수
+	 */
+	public int insert(User user);
+	
+	/**
+	 * 아이디 존재 확인
+	 * @param id 찾을 아이디
+	 * @return	해당 아이디 존재 여부 (있으면 true)
+	 */
+	public boolean emailcheck(String email);
+	
+	/**
+	 * 아이디로 회원 정보 찾기
+	 * @param id 검색할 아이디
+	 * @return	해당 회원의 정보
+	 */
+	public User getUser(String email);
+	
+	
+}
---
+++ emiary/src/main/java/com/emiary/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.emiary.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.emiary.dao.UserDAO;
+import com.emiary.domain.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired
+	UserDAO dao;
+	
+	@Autowired
+	PasswordEncoder encoder;
+	
+	@Override
+	public int insert(User user) {
+		//비밀번호 암호화
+		String pw = encoder.encode(user.getPassword());
+		user.setPassword(pw);		
+		
+		int n = dao.insert(user);
+		return n;
+	}
+
+	@Override
+	public boolean emailcheck(String email) {
+		
+		return dao.select(email) != null;
+	}
+
+	@Override
+	public User getUser(String email) {
+		User user = dao.select(email);
+		return user;
+	}
+
+
+	
+	
+
+}
