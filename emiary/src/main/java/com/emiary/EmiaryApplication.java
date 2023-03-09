114fc483f3e9aa26b30a9bd2a50519f258799a9d
leeseungyeong <onunisuki62@gmail.com>
2023-03-09 09:25:16 +0900

    emairy 초기세팅

---
+++ emiary/src/main/java/com/emiary/EmiaryApplication.java
@@ -0,0 +1,13 @@
+package com.emiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmiaryApplication.class, args);
+	}
+
+}
---
+++ emiary/src/test/java/com/emiary/EmiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
