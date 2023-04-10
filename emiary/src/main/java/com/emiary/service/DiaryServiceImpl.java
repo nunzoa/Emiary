177dc023cc4f824ba87ce58119df403f9566d410
dev-yeon <110480577+dev-yeon@users.noreply.github.com>
2023-04-10 12:25:34 +0900

    JSON parsing TEST!

--- emiary/src/main/java/com/emiary/service/DiaryServiceImpl.java
+++ emiary/src/main/java/com/emiary/service/DiaryServiceImpl.java
@@ -9,6 +9,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.cloudinary.json.JSONArray;
 import org.cloudinary.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,9 @@ import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+
+//Json Parsing
+
 
 @Slf4j
 @Service
@@ -44,24 +48,63 @@ public class DiaryServiceImpl implements DiaryService {
 
 		httpPost.setHeader("Content-type", "application/json");
 		//서버에 요청 할 때, 나의 컨텐츠 타입을 "application/json"
-
 		JSONObject jsonObject = new JSONObject();
-
 		jsonObject.put("keyword", keyword);
-
+		// 빈출 단어 =keyword
 		StringEntity stringEntity = new StringEntity(jsonObject.toString(), "UTF-8");
 
 		httpPost.setEntity(stringEntity);
-
 		HttpResponse httpResponse = httpClient.execute(httpPost);
 		httpResponse.setHeader("Content-type", "application/json");
 
 		String responseBody = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
 
 		String output = StringEscapeUtils.unescapeJava(responseBody);
+		// 가장 큰 JSONObject를 가져옵니다.
+		JSONObject responseJson = new JSONObject(responseBody);
+		// 첫번째 JSONObject를 가져와서 key-value를 읽습니다.
+		JSONObject _index = responseJson.getJSONObject("_index");
+		System.out.println(_index.toString());
 
+		String _id =  _index.getString("_id");
+		String _score = _index.getString("_score");
+		System.out.println("_id(_index): " + _id);
+		System.out.println("_score(_index): " + _score);
+
+		// 두번째 JSONObject를 가져와서 key-value를 읽습니다.
+		JSONObject _source = responseJson.getJSONObject("_source");
+		System.out.println(_source.toString());
+		System.out.println();
+
+		String post_desc =_source.getString("post_desc");
+		String author_id = _source.getString("author_id");
+		String like_string = _source.getString("like_string");
+		String post_tag = _source.getString("post_tag");
+
+		System.out.println("post_desc(source)"+post_desc);
+		System.out.println("author_id(source)"+author_id);
+		System.out.println("like_string(source)"+like_string);
+		System.out.println("post_tag(source)"+post_tag);
+		// 배열을 가져옵니다.
+		JSONArray JsonArray = responseJson.getJSONArray("img_list");
+		// 배열의 모든 아이템을 출력합니다.
+		for (int i = 0; i < JsonArray.length(); i++) {
+			JSONObject Jsonimglist = JsonArray.getJSONObject(i);
+			String img_url = Jsonimglist.getString("img_url");
+			String img_alt = Jsonimglist.getString("img_alt");
+			String img_name = Jsonimglist.getString("img_name");
+			String img_tag = Jsonimglist.getString("img_tag");
+
+			System.out.println("img_url(" + i + "): " + img_url);
+			System.out.println("img_alt(" + i + "): " + img_alt);
+			System.out.println("img_name(" + i + "): " + img_name);
+			System.out.println("img_tag(" + i + "): " + img_tag);
+			System.out.println();
+		}
 		return output;
 	}
+
+
 	@Override
 	public Diaries read(String dayString, String username) {
 		Map<String, String> map = new HashMap<>();
